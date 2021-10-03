d5443457afab0f0020961cca2fbae134e59899f8
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-10-03 12:15:56 +0000

    auto: black code format

--- userge/plugins/tools/save.py
+++ userge/plugins/tools/save.py
@@ -1,8 +1,9 @@
-#New Update Soon😅
-from userge import Config, Message, userge
+# New Update Soon😅
+from userge import Message, userge
 
-@userge.on_cmd(
-    "save", about={"header": "Save message"}, allow_channels=True
-)
+
+@userge.on_cmd("save", about={"header": "Save message"}, allow_channels=True)
 async def savemsg(message: Message):
-    await userge.forward_messages("me",message.chat.id,message.reply_to_message.message_id)
+    await userge.forward_messages(
+        "me", message.chat.id, message.reply_to_message.message_id
+    )
