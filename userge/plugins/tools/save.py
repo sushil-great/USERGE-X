75bfb8336e275c1beb6a458b80b138d92a972f24
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-10-03 11:37:13 +0000

    auto: black code format

--- userge/plugins/tools/save.py
+++ userge/plugins/tools/save.py
@@ -1,6 +1,9 @@
-#New Update Soon😅
-from userge import Config,userge
+# New Update Soon😅
+from userge import Config, userge
 
-@userge.on_message(filters.command("save",[Config.CMD_TRIGGER,Config.SUDO_TRIGGER]))
+
+@userge.on_message(filters.command("save", [Config.CMD_TRIGGER, Config.SUDO_TRIGGER]))
 async def savemsg(userge, message):
-    await userge.forward_messages("me",message.chat.id,message.reply_to_message.message_id)
+    await userge.forward_messages(
+        "me", message.chat.id, message.reply_to_message.message_id
+    )
